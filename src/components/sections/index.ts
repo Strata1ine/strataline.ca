b52e00beb975d75180404746379d9d3f28693f0e
thefiredman <haimovitzsh@gmail.com>
2025-03-27 19:51:27 -0400

    fix: insane 'typo'

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -9,7 +9,7 @@ import Question from "./Question.astro";
 import ZigZag from "./ZigZag.astro";
 import Faq from "./Faq.astro";
 import Panel from "./Panel.astro";
-import Messege from "./Messege.astro";
+import Message from "./Message.astro";
 import Markdown from "./Markdown.astro";
 
-export { Hero, Popular, LessPopular, ImageCarousel, Review, TextCarousel, Slide, Question, ZigZag, Faq, Panel, Markdown, Messege };
+export { Hero, Popular, LessPopular, ImageCarousel, Review, TextCarousel, Slide, Question, ZigZag, Faq, Panel, Markdown, Message };
