b7d4f52a7f8cea98c9b2dbfcb53de842d57afdd0
thefiredman <haimovitzsh@gmail.com>
2025-05-30 13:50:54 -0400

    fix: bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,11 +16,40 @@ export default defineConfig({
     // responsiveImages: true,
   },
   compressHTML: true,
-  vite: { plugins: [tailwindcss()], },
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        external: ['sharp']
+      }
+    },
+    optimizeDeps: {
+      exclude: ['sharp']
+    },
+    ssr: {
+      external: ['sharp']
+    }
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        quality: 20,
+      },
+    },
+  },
   integrations: [svelte(), compressor(), (await import("@playform/compress")).default({
     CSS: true,
-    HTML: false,
-    Image: false,
+    HTML: {
+      "html-minifier-terser": {
+        removeEmptyAttributes: false,
+        removeComments: false,
+        removeAttributeQuotes: false,
+        collapseWhitespace: false,
+        conservativeCollapse: true,
+      }
+    },
+    Image: true,
     JavaScript: true,
     JSON: true,
     SVG: true,
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,2 @@
 import Hero from "@sections/Hero.astro";
-import Slideshow from "@interact/Slideshow.svelte";
-import Slidenav from "@interact/Slidenav.svelte";
-import Button from "@actions/Button.svelte";
-import Link from "@actions/Link.svelte";
-export { Hero, Slideshow, Slidenav, Button, Link };
+export { Hero };
