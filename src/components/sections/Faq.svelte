e063f0f2ec20a1e70ffdcee33470bf106b642c51
thefiredman <44515019+thefiredman@users.noreply.github.com>
2025-07-25 18:08:25 -0400

    impl: faq

---
+++ src/components/sections/Faq.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { type PropsOf } from "./registry";
+  const { meta }: { meta: PropsOf<"Faq"> } = $props();
+
+  let active: null | number = $state(null);
+  import FaqItem from "./FaqItem.svelte";
+</script>
+
+<div class="-my-4">
+  {#each meta.content as faq, i}
+    <FaqItem meta={faq} {i} bind:active></FaqItem>
+  {/each}
+</div>
