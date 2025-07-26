631836fb5aed6643f848890f9d34df653d95a9b8
da-shalev <44515019+da-shalev@users.noreply.github.com>
2025-07-26 16:30:50 -0400

    fix: stupid me

---
+++ src/components/sections/pos.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const ZPos = z.union([z.literal('left'), z.literal('right')]);
+export const DefaultPos = ZPos.default("left");
+export type Pos = z.infer<typeof ZPos>;
+
+export function swapNextPos(pos: Pos) {
+  pos = swapPos(pos);
+}
+
+export function swapPos(pos: Pos): Pos {
+  return pos === "left" ? "right" : "left"
+}
