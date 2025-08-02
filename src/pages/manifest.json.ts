c68408b88dc3366dc9517c11aa57f90be4192525
da-shalev <44515019+da-shalev@users.noreply.github.com>
2025-08-02 16:27:12 -0400

    impl: favicons

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,36 @@
+import type { APIRoute } from "astro";
+import { getImage } from "astro:assets";
+
+import favicon from "~/icons/favicon.png";
+const sizes = [32, 180, 512];
+
+import business from "@root/content/business.json";
+
+export const pngIcons = Promise.all(
+  sizes.map(async (size: number) => {
+    const image = await getImage({
+      src: favicon,
+      width: size,
+      height: size,
+      format: "png",
+    });
+
+    return {
+      src: image.src,
+      type: image.options.format,
+      sizes: `${image.options.width}x${image.options.height}`,
+    };
+  }),
+);
+
+export const GET: APIRoute = async () => {
+  const manifest = {
+    name: business.name,
+    description: business.description,
+    start_url: "/",
+    display: "standalone",
+    icons: await pngIcons,
+  };
+
+  return new Response(JSON.stringify(manifest));
+};
