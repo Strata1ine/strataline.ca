92a53c3c0ba55c9238dfec9792c0d70866d6c55a
thefiredman <44515019+thefiredman@users.noreply.github.com>
2025-08-05 17:25:18 -0400

    production ready

---
+++ src/components/decor/Talk.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Mailbox from "~/icons/ph/mailbox-fill.svelte";
+
+  import { modals } from "~/frontend/stores.svelte";
+  import { actionStyles } from "@actions/styles";
+</script>
+
+<button
+  class="{actionStyles({
+    variant: 'pill',
+    background: 'accent',
+  })} fixed right-5 bottom-7 z-1 flex items-center justify-center sm:right-13 sm:bottom-15"
+  onclick={() => modals.open(modals.talk)}
+>
+  <Mailbox class="size-11" />
+</button>
